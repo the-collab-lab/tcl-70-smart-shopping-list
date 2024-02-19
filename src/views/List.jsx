3338f345b38768c9b7f787afa0df345fdd345611
amandaguan-ag <amandaguan1314@gmail.com>
2024-02-19 13:22:30 -0800

    add search form

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react';
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const handleChange = (e) => {
+		setSearchTerm(e.target.value);
+		console.log('Changed!');
+	};
+
+	const clearSearch = () => {
+		setSearchTerm('');
+		console.log('CLEARED!');
+	};
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<form>
+				<label htmlFor="itemSearch">Search:</label>
+				<input
+					type="text"
+					id="itemSearch"
+					placeholder="Search items..."
+					value={searchTerm}
+					onChange={handleChange}
+				></input>
+				{searchTerm && (
+					<button type="button" onClick={clearSearch}>
+						Clear
+					</button>
+				)}
+			</form>
 			<ul>
 				{/* Renders the `data` array using the `ListItem` component that's imported at the top of this file.*/}
 				{data.map((item) => {
