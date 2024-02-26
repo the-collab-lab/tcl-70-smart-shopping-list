156cec3b991101da4496290ac6bd31139314403d
Leon <leon.karho.chung@gmail.com>
2024-02-26 16:31:42 -0500

    fix updateItem triggered on change - useEffect to track isCheck state, and updateItem if true

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,14 +1,23 @@
 import './ListItem.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { updateItem } from '../api/firebase';
 
 export function ListItem({ name, listPath, itemId }) {
 	const [isChecked, setIsChecked] = useState(false);
 
-	const handleChange = () => {
+	const handleChange = async () => {
 		setIsChecked(!isChecked);
-		updateItem(listPath, itemId);
 	};
+
+	useEffect(() => {
+		try {
+			if (isChecked) {
+				updateItem(listPath, itemId);
+			}
+		} catch (error) {
+			console.log('error: ', error);
+		}
+	}, [isChecked]);
 
 	return (
 		<li className="ListItem">
