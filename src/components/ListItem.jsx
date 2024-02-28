2c47befe79e2c05785abcde0cbffdaf4080b350e
Leon <leon.karho.chung@gmail.com>
2024-02-28 16:49:18 -0500

    use dynamic label and input name - useEffect to calculate expiryDate, if within 24 hours, user cannot unclick item, if past 24 hours, user can

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,18 +2,36 @@ import './ListItem.css';
 import { useEffect, useState } from 'react';
 import { updateItem } from '../api/firebase';
 
-export function ListItem({ name, listPath, itemId }) {
+export function ListItem({ name, listPath, itemId, dateLastPurchased }) {
 	const [isChecked, setIsChecked] = useState(false);
+	const [expired, setExpired] = useState();
 
 	const handleChange = async () => {
 		setIsChecked(!isChecked);
 	};
 
 	useEffect(() => {
-		try {
-			if (isChecked) {
-				updateItem(listPath, itemId);
+		if (dateLastPurchased) {
+			const expiryDate = new Date(dateLastPurchased.seconds * 1000 + 86400000);
+
+			if (new Date() > expiryDate) {
+				setIsChecked(false);
+				setExpired(false);
+			} else {
+				setIsChecked(true);
+				setExpired(true);
 			}
+		}
+	}, [dateLastPurchased.seconds]);
+
+	useEffect(() => {
+		const handleUpdateItem = async () => {
+			if (isChecked) {
+				await updateItem(listPath, itemId);
+			}
+		};
+		try {
+			handleUpdateItem();
 		} catch (error) {
 			console.log('error: ', error);
 		}
@@ -21,8 +39,14 @@ export function ListItem({ name, listPath, itemId }) {
 
 	return (
 		<li className="ListItem">
-			<label>
-				<input type="checkbox" checked={isChecked} onChange={handleChange} />
+			<label htmlFor={`${name}`}>
+				<input
+					disabled={expired}
+					id={`${name}`}
+					type="checkbox"
+					checked={isChecked}
+					onChange={handleChange}
+				/>
 				{name}
 			</label>
 		</li>
