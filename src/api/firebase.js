f23b2db710aad17024b22c5019b74a6063035e51
amandaguan-ag <amandaguan1314@gmail.com>
2024-03-14 17:25:37 -0700

    remove uncessary comment

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -238,15 +238,7 @@ export async function deleteItem(listPath, itemId) {
 
 export function comparePurchaseUrgency(a, b) {
 	const daysPassedA = a.daysUntilNextPurchase;
-	// const daysPassedA = getDaysBetweenDates(
-	// 	new Date(),
-	// 	a.dateNextPurchased.toDate(),
-	// );
 	const daysPassedB = b.daysUntilNextPurchase;
-	// const daysPassedB = getDaysBetweenDates(
-	// 	new Date(),
-	// 	b.dateNextPurchased.toDate(),
-	// );
 	if (daysPassedA < daysPassedB) {
 		return -1;
 	}
