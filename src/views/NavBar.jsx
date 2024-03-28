ba6f3ef5e043f5b37fd321cfe3e9a070f6d45975
MiliMade <milimadeemail@gmail.com>
2024-03-28 18:06:31 -0400

    Made navbar responsive

--- src/views/NavBar.jsx
+++ src/views/NavBar.jsx
@@ -8,42 +8,55 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { NavLink } from 'react-router-dom';
-
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import SvgIcon from '@mui/material/SvgIcon';
 
 function NavBar() {
 	const { user } = useAuth();
 	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
 
 	const handleOpenNavMenu = (event) => {
 		setAnchorElNav(event.currentTarget);
-	};
-	const handleOpenUserMenu = (event) => {
-		setAnchorElUser(event.currentTarget);
 	};
 
 	const handleCloseNavMenu = () => {
 		setAnchorElNav(null);
 	};
 
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
-
 	return (
 		<AppBar position="static">
 			<Container maxWidth="xl">
-				<Toolbar disableGutters>
+				<Toolbar variant="dense" disableGutters>
 					{/*Logo and app name for medium or larger screens*/}
-					<AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+					<SvgIcon
+						sx={{
+							fontSize: '20px',
+							display: { xs: 'none', md: 'flex' },
+							mr: 1,
+						}}
+					>
+						<svg
+							height="200px"
+							width="200px"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 290.87 290.87"
+							xmlSpace="preserve"
+							fill="#000"
+							transform="scale(-1 1)"
+							stroke="#000"
+							strokeWidth={0.00290873}
+						>
+							<g fill="#010002">
+								<path d="M44.268 139.532L44.268 79.35 44.268 41.049 93.507 90.294z" />
+								<path d="M0 73.384L32.334 41.049 32.334 73.384z" />
+								<path d="M149.583 249.824L113.508 213.748 149.583 181.843z" />
+								<path d="M103.066 202.799L47.216 153.823 160.968 48.77 290.873 48.77z" />
+							</g>
+						</svg>
+					</SvgIcon>
 					<Typography
 						variant="h6"
 						noWrap
@@ -59,30 +72,8 @@ function NavBar() {
 							textDecoration: 'none',
 						}}
 					>
-						LOGO
+						SwiftShop
 					</Typography>
-
-					{/* app logo for small screens */}
-					<AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-					<Typography
-						variant="h5"
-						noWrap
-						component="a"
-						href="#app-bar-with-responsive-menu"
-						sx={{
-							mr: 2,
-							display: { xs: 'flex', md: 'none' },
-							flexGrow: 1,
-							fontFamily: 'monospace',
-							fontWeight: 700,
-							letterSpacing: '.3rem',
-							color: 'inherit',
-							textDecoration: 'none',
-						}}
-					>
-						LOGO
-					</Typography>
-
 					{/* nav links for larger screens */}
 					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
 						<NavLink to="/" className="Nav-link">
@@ -111,7 +102,59 @@ function NavBar() {
 						</NavLink>
 					</Box>
 
-					<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+					{/* app logo for small screens */}
+					<SvgIcon
+						sx={{
+							fontSize: '20px',
+							display: { xs: 'flex', md: 'none' },
+							mr: 1,
+						}}
+					>
+						<svg
+							height="200px"
+							width="200px"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 290.87 290.87"
+							xmlSpace="preserve"
+							fill="#000"
+							transform="scale(-1 1)"
+							stroke="#000"
+							strokeWidth={0.00290873}
+						>
+							<g fill="#010002">
+								<path d="M44.268 139.532L44.268 79.35 44.268 41.049 93.507 90.294z" />
+								<path d="M0 73.384L32.334 41.049 32.334 73.384z" />
+								<path d="M149.583 249.824L113.508 213.748 149.583 181.843z" />
+								<path d="M103.066 202.799L47.216 153.823 160.968 48.77 290.873 48.77z" />
+							</g>
+						</svg>
+					</SvgIcon>
+					<Typography
+						variant="h5"
+						noWrap
+						component="a"
+						href="#app-bar-with-responsive-menu"
+						sx={{
+							mr: 2,
+							display: { xs: 'flex', md: 'none' },
+							flexGrow: 1,
+							fontFamily: 'monospace',
+							fontWeight: 700,
+							letterSpacing: '.3rem',
+							color: 'inherit',
+							textDecoration: 'none',
+						}}
+					>
+						SwiftShop
+					</Typography>
+					{/* nav links for smaller screens */}
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: 'flex', md: 'none' },
+							justifyContent: 'end',
+						}}
+					>
 						<IconButton
 							size="large"
 							aria-label="account of current user"
@@ -159,6 +202,7 @@ function NavBar() {
 						</Menu>
 					</Box>
 
+					{/* username and signin/signout button for larger screens */}
 					<Box
 						sx={{
 							flexGrow: 1,
@@ -176,17 +220,28 @@ function NavBar() {
 						)}
 					</Box>
 				</Toolbar>
-				<Toolbar sx={{ display: { xs: 'flex', md: 'none' } }}>
+
+				{/* username and signin/signout button for smaller screens */}
+				<Toolbar
+					variant="dense"
+					disableGutters
+					sx={{ display: { xs: 'flex', md: 'none' }, padding: 0 }}
+				>
 					<Box
 						sx={{
 							flexGrow: 1,
 							flexDirection: 'row',
-							display: { xs: 'none', md: 'flex' },
+							display: { xs: 'flex', md: 'none' },
+							justifyContent: 'start',
 						}}
 					>
-						<Typography>
-							Signed in as <Button sx={{ color: 'white' }}>Sign out</Button>
-						</Typography>
+						{!!user ? (
+							<Typography>
+								Signed in as {user.displayName} <SignOutButton />
+							</Typography>
+						) : (
+							<SignInButton />
+						)}
 					</Box>
 				</Toolbar>
 			</Container>
