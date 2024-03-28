90310d884f357a8ef10f659d9650ed328acb7685
MiliMade <milimadeemail@gmail.com>
2024-03-28 19:46:41 -0400

    re-add layout file

---
+++ src/views/Layout.jsx
@@ -0,0 +1,51 @@
+import { Outlet, NavLink } from 'react-router-dom';
+
+import './Layout.css';
+import { auth } from '../api/config.js';
+import { SignInButton, SignOutButton, useAuth } from '../api/useAuth.jsx';
+
+/**
+ * TODO: The links defined in this file don't work!
+ *
+ * Instead of anchor element, they should use a component
+ * from `react-router-dom` to navigate to the routes
+ * defined in `App.jsx`.
+ */
+
+export function Layout() {
+	const { user } = useAuth();
+
+	return (
+		<>
+			<div className="Layout">
+				<header className="Layout-header">
+					<h1>Smart shopping list</h1>
+					{!!user ? (
+						<span>
+							<p>Signed in as {user.displayName}</p>
+							<SignOutButton />
+						</span>
+					) : (
+						<SignInButton />
+					)}
+				</header>
+				<main className="Layout-main">
+					<Outlet />
+				</main>
+				<nav className="Nav">
+					<div className="Nav-container">
+						<NavLink to="/" className="Nav-link">
+							Home
+						</NavLink>
+						<NavLink to="/list" className="Nav-link">
+							List
+						</NavLink>
+						<NavLink to="/manage-list" className="Nav-link">
+							Manage List
+						</NavLink>
+					</div>
+				</nav>
+			</div>
+		</>
+	);
+}
