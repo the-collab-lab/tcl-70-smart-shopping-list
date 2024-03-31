1b03fc31f2a3d0d19bd68d6e0a154dc7dc83494d
amandaguan-ag <amandaguan1314@gmail.com>
2024-03-31 12:57:49 -0700

    remove useMemo

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -1,38 +1,32 @@
-import { createContext, useContext, useMemo, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import {
 	createTheme,
 	ThemeProvider as MUIThemeProvider,
+	CssBaseline,
 } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
 
-const ThemeContext = createContext();
+const ThemeContext = createContext({
+	// prevent error of attempt to use toggleColorMode without the ThemeProvider properly set up
+	toggleColorMode: () => {},
+});
 
 export const useThemeContext = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
 	const [mode, setMode] = useState('light');
 
-	const colorMode = useMemo(
-		() => ({
-			toggleColorMode: () => {
-				setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-			},
-		}),
-		[],
-	);
+	const toggleColorMode = () => {
+		setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+	};
 
-	const theme = useMemo(
-		() =>
-			createTheme({
-				palette: {
-					mode,
-				},
-			}),
-		[mode],
-	);
+	const theme = createTheme({
+		palette: {
+			mode,
+		},
+	});
 
 	return (
-		<ThemeContext.Provider value={colorMode}>
+		<ThemeContext.Provider value={{ toggleColorMode }}>
 			<MUIThemeProvider theme={theme}>
 				<CssBaseline />
 				{children}
