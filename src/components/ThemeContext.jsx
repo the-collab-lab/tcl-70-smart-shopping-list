54abe7cb8695cdb50db3b8039863feee721d11e7
amandaguan-ag <amandaguan1314@gmail.com>
2024-03-31 13:43:26 -0700

    modify CssBaseline import and add comments

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -2,23 +2,24 @@ import { createContext, useContext, useState } from 'react';
 import {
 	createTheme,
 	ThemeProvider as MUIThemeProvider,
-	CssBaseline,
 } from '@mui/material/styles';
-
+import { CssBaseline } from '@mui/material';
+// uses React's Context API to create a global theme state
 const ThemeContext = createContext({
 	// prevent error of attempt to use toggleColorMode without the ThemeProvider properly set up
 	toggleColorMode: () => {},
 });
-
+// custom hook
 export const useThemeContext = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
+	// state for theme mode
 	const [mode, setMode] = useState('light');
-
+	// theme mode toggle function
 	const toggleColorMode = () => {
 		setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
 	};
-
+	// MUI theme creation
 	const theme = createTheme({
 		palette: {
 			mode,
@@ -26,6 +27,7 @@ export const ThemeProvider = ({ children }) => {
 	});
 
 	return (
+		// Providing the Context and Theme
 		<ThemeContext.Provider value={{ toggleColorMode }}>
 			<MUIThemeProvider theme={theme}>
 				<CssBaseline />
