2aaacb74bacf042a37de2378d0e266debb6b820c
MiliMade <milimadeemail@gmail.com>
2024-04-02 22:11:02 -0400

    Made the header smaller, cleaned up old code, fixed error in console

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import SvgIcon from '@mui/material/SvgIcon';
 
 import './Layout.css';
@@ -6,33 +6,15 @@ import './Layout.css';
 // import { SignInButton, SignOutButton, useAuth } from '../api/useAuth.jsx';
 import { NavBar } from '../components';
 
-/**
- * TODO: The links defined in this file don't work!
- *
- * Instead of anchor element, they should use a component
- * from `react-router-dom` to navigate to the routes
- * defined in `App.jsx`.
- */
-
 export function Layout() {
-	// const { user } = useAuth();
-
 	return (
 		<>
-			<head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-					rel="stylesheet"
-				/>
-			</head>
 			<div className="Layout">
 				<header className="Layout-header">
 					<img src="/img/SwiftShopBird.png" alt="" />
 					<h1>
-						<span className="swift-header">Swift</span>
-						<span className="shop-header">shop</span>
+						<p className="swift-header">Swift</p>
+						<p className="shop-header">shop</p>
 					</h1>
 
 					{/* For use later
